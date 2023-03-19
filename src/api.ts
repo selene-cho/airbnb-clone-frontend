75001a0ac466eae67ca3e8890467066aa65dfad9
selene-cho <selene.cho29@gmail.com>
2023-03-19 21:00:23 +0900

    feat: Reviews

--- src/api.ts
+++ src/api.ts
@@ -12,3 +12,10 @@ export const getRoom = ({ queryKey }: QueryFunctionContext) => {
   const [, roomPk] = queryKey; // ${roomPk} 대신 바로 ${queryKey[1]} 이런 식으로 써도 되지만 queryKey안에 roomPk 있다고 협업 위해 명시하는 것이 좋음.
   return instance.get(`rooms/${roomPk}`).then((response) => response.data);
 };
+
+export const getRoomReviews = ({ queryKey }: QueryFunctionContext) => {
+  const [, roomPk] = queryKey; // ${roomPk} 대신 바로 ${queryKey[1]} 이런 식으로 써도 되지만 queryKey안에 roomPk 있다고 협업 위해 명시하는 것이 좋음.
+  return instance
+    .get(`rooms/${roomPk}/reviews`)
+    .then((response) => response.data);
+};
--- src/routes/RoomDetail.tsx
+++ src/routes/RoomDetail.tsx
@@ -1,20 +1,27 @@
 import {
+  Avatar,
   Box,
   Grid,
   GridItem,
   Heading,
+  HStack,
   Image,
   Skeleton,
+  Text,
+  VStack,
 } from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
+import { FaStar } from 'react-icons/fa';
 import { useParams } from 'react-router-dom';
-import { getRoom } from '../api';
-import { IRoomDetail } from '../types';
+import { getRoom, getRoomReviews } from '../api';
+import { IReview, IRoomDetail } from '../types';
 
 export default function RoomDetail() {
   const { roomPk } = useParams(); //uesParams : 해당 URL에 있는 파라미터 가져다 줌.
   const { isLoading, data } = useQuery<IRoomDetail>([`rooms`, roomPk], getRoom); // 데이터를 캐시에다, key를 써서 저장해야 함.
-
+  const { data: reviewsData, isLoading: isReviewsLoading } = useQuery<
+    IReview[]
+  >([`rooms`, roomPk, `reviews`], getRoomReviews);
   return (
     <Box
       mt={10}
@@ -53,6 +60,39 @@ export default function RoomDetail() {
           </GridItem>
         ))}
       </Grid>
+      <HStack width={'40%'} justifyContent={'space-between'} mt={10}>
+        <VStack alignItems={'flex-start'}>
+          <Skeleton isLoaded={!isLoading} height={'30px'}>
+            <Heading fontSize={'2xl'}>
+              House hosted by {data?.owner.name}
+            </Heading>
+          </Skeleton>
+          <Skeleton isLoaded={!isLoading} height={'30px'}>
+            <HStack justifyContent={'flex-start'} w="100%">
+              <Text>
+                {data?.toilets} toilet{data?.toilets === 1 ? '' : 's'}
+              </Text>
+              <Text>•</Text>
+              <Text>
+                {data?.rooms} room{data?.rooms === 1 ? '' : 's'}
+              </Text>
+            </HStack>
+          </Skeleton>
+        </VStack>
+        <Avatar name={data?.owner.name} size={'xl'} src={data?.owner.avatar} />
+      </HStack>
+      <Box mt={10}>
+        <Heading fontSize={'2xl'}>
+          <HStack>
+            <FaStar /> <Text>{data?.rating}</Text>
+            <Text>•</Text>
+            <Text>
+              {reviewsData?.length} review
+              {reviewsData?.length === 1 ? '' : 's'}
+            </Text>
+          </HStack>
+        </Heading>
+      </Box>
     </Box>
   );
 }
